96ec2e50020d33b70578fb8faff457c29989e13d
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2014-02-04 02:02:24 +0900

    fetch first 1500 tweet of user

--- app.rb
+++ app.rb
@@ -145,8 +145,17 @@ get '/tweets.json' do
   end
   tweets = []
   if screen_name
-     client.user_timeline(screen_name, options).each do |tweet|
-      #if tweet.in_reply_to_screen_name == 'posterdone'
+    tweet_objects = []
+    loop do
+      options[:max_id] = tweet_objects.last.id.to_i-1 if tweet_objects.count > 0
+      _tweet_objects = client.user_timeline(screen_name, options)
+      pp _tweet_objects
+      p _tweet_objects.count
+      break if _tweet_objects.count <= 0
+      tweet_objects += _tweet_objects
+      break if tweet_objects.count > 1000
+    end
+    tweet_objects.each do |tweet|
       if tweet.text =~ /posterdone/
         if tweet.favorited
           redis.hset(REDIS_KEY, tweet.id.to_s, true)
@@ -156,9 +165,42 @@ get '/tweets.json' do
             t[:photo_uri] = tweet.media[0].media_uri.to_s
           end
           tweets << t
-        end
-      end
-    end   
+        end # if
+      end # if
+    end
+    pp tweets
+
+    #loop do
+    #  _tweets = []
+    #  p tweets
+    #  if tweets.count > 0
+    #    options[:max_id] = tweets.last[:id].to_i - 1
+    #  end
+    #  begin
+    #    client.user_timeline(screen_name, options).each do |tweet|
+    #      #if tweet.in_reply_to_screen_name == 'posterdone'
+    #      if tweet.text =~ /posterdone/
+    #        if tweet.favorited
+    #          redis.hset(REDIS_KEY, tweet.id.to_s, true)
+    #        elsif !redis.hget(REDIS_KEY, tweet.id.to_s)
+    #          t = {:id => tweet.id.to_s, :uri => tweet.uri, :text => tweet.text, :favorited => tweet.favorited}
+    #          if tweet.media[0]
+    #            t[:photo_uri] = tweet.media[0].media_uri.to_s
+    #          end
+    #          _tweets << t
+    #        end # if
+    #      end # if
+    #    end #do
+    #  rescue => e
+    #     e
+    #    break
+    #  end
+    #  puts "#{screen_name}: #{_tweets.count}"
+    #  if _tweets.count <= 0
+    #    break
+    #  end
+    #  tweets += _tweets
+    #end # loop
   else
     client.search("posterdone #{filter_text}", options).each do |tweet|
       if !redis.hget(REDIS_KEY, tweet.id.to_s)
